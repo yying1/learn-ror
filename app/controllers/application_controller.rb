2c986510238f4974b0a807e1b4fb1dc5ed260834
Frank Yue Ying <yingyuefrank@gmail.com>
2023-01-09 20:56:24 +0000

    create /rock for RPS game

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  # this add layout to all pages
+  layout("wrapper.html.erb")
+
+  def homepage
+    render({ :template => "game_templates/rules.html.erb", :layout => "wrapper.html.erb" })
+  end
+
+  def play_rock
+    @share_var = "testing shared variable"
+    render({ :template => "game_templates/user_rock.html.erb" })
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
+  get("/", { :controller => "application", :action => "homepage" })
 
-
-
+  get("/rock", { :controller => "application", :action => "play_rock" })
 end
